ebb61b86ddb52e1fb9615d3b05e6e9e5acc5674a
Alexander Meise <alexander.meise@capside.com>
2022-03-27 12:43:11 +0200

    remove spaces

--- curler.sh
+++ curler.sh
@@ -5,7 +5,7 @@ curl -o /dev/null -s -w "@format-x.txt" -X POST --data '{
   "params" :{
       "address":"X-avax1k276wp8jdu79jakxfjkefhx3m424q20pp5gfka"
   }
-}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-x | curl --data-binary @- http://localhost:9091                    /metrics/job/job
+}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-x | curl --data-binary @- http://localhost:9091/metrics/job/job
 
 curl -o /dev/null -s -w "@format-p.txt" -X POST --data '{
   "jsonrpc":"2.0",
@@ -14,7 +14,7 @@ curl -o /dev/null -s -w "@format-p.txt" -X POST --data '{
   "params" :{
       "address":"P-avax1fmlnu36mmxxweyn2zsdrhqpf42puqz97gwsaw3"
   }
-}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-p | curl --data-binary @- http://localhost:9091                    /metrics/job/job
+}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-p | curl --data-binary @- http://localhost:9091/metrics/job/job
 
 curl -o /dev/null -s -w "@format-c.txt" -X POST --data '{
     "jsonrpc": "2.0",
@@ -25,5 +25,5 @@ curl -o /dev/null -s -w "@format-c.txt" -X POST --data '{
         "3RvKBAmQnfYionFXMfW5P8TDZgZiogKbHjM8cjpu16LKAgF5T"
     ],
     "id": 1
-}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-c | curl --data-binary @- http://localhost:9091                    /metrics/job/job
+}' -H 'content-type:application/json;' https://rpc.ankr.com/avalanche-c | curl --data-binary @- http://localhost:9091/metrics/job/job
 
